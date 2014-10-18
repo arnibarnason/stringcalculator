75c42710070e202e94d154c15d0575b9d536b1c2
Arni Benedikt Arnason <arnibarnason@gmail.com>
2014-10-18 15:29:12 +0000

    Handling different delimiters

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,10 +1,17 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
+	
+	private static String delimmiter;
 
 	public static int add(String text){
 		if(text.equals("")){
 			return 0;
+		}
+		
+		else if(text.startsWith("//")){
+			delimmiter = String.valueOf(text.charAt(2));;
+			return sum(splitCustomDelim(text));
 		}
 
 		else if(text.contains(",") || text.contains("\n")){
@@ -20,12 +27,18 @@ public class Calculator {
 	}
       
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",|\\n");
+		return numbers.split(",|\\n");
 	}
-    private static int sum(String[] numbers){
- 	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
+
+	private static String[] splitCustomDelim(String numbers){
+		numbers = numbers.substring(4, numbers.length());
+		return numbers.split(delimmiter);
+	}
+
+	private static int sum(String[] numbers){
+ 		int total = 0;
+        	for(String number : numbers){
+			total += toInt(number);
 		}
 		return total;
     }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -32,6 +32,11 @@ public class CalculatorTest {
 	@Test
 	public void testNewLineAndCommaDelimmiter(){
     	assertEquals(6, Calculator.add("1\n2,3"));
+   	}
+
+	@Test
+	public void testCustomDelimiter(){
+    	assertEquals(3, Calculator.add("//;\n1;2"));
     }
 
 }
